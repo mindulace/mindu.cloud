0c6ae61fbab4dd541fb2d8592d431ff44e0c4097
Hendrik Bonke <hbonke@gmx.net>
2020-05-23 10:56:11 +0200

    adding bootstrap

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,48 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+    <div id="app">
+        <div id="nav">
+            <router-link to="/">Home</router-link> |
+            <router-link to="/about">About</router-link>
+        </div>
+        <transition name="fade" mode="out-in">
+            <router-view/>
+        </transition>
     </div>
-    <router-view/>
-  </div>
 </template>
 
 <style>
+.fade-enter-active, .fade-leave-active {
+    transition: opacity .5s;
+}
+.fade-enter, .fade-leave-to /* .fade-leave-active below version 2.1.8 */ {
+    opacity: 0;
+}
+
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+    font-family: 'Avenir', Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #f3f3f3;
+    background: #252525;
+    border-radius: 5px
+}
+
+body {
+    margin: 15px;
+    background: #333333;
 }
 
 #nav {
-  padding: 30px;
+    padding: 30px;
 }
 
 #nav a {
-  font-weight: bold;
-  color: #2c3e50;
+    font-weight: bold;
+    color: #2c3e50;
 }
 
 #nav a.router-link-exact-active {
-  color: #42b983;
+    color: #42b983;
 }
 </style>
---
+++ src/components/CategoryTeaser.vue
@@ -0,0 +1,20 @@
+<template>
+    <div class="category--teaser" style="width: 30%;">
+        
+    </div>
+</template>
+
+<script>
+export default {
+name: 'CategoryTeaser',
+    props: {
+        msg: String,
+        link: String,
+        linkText: String
+    }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+</style>
